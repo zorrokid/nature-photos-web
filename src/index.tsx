6ef405d1b6f32f075115664c9e3a8eabbc3949ec
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-09-25 00:54:22 +0300

    Add Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from './firebaseConfig';
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
