d666e878e299c5e95f578cce13b0b1deaea95fb1
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-09-29 23:52:33 +0300

    list photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
+  const photos = useAppSelector((state) => state.photos.photos);
   return (
-    <div>
-    	Nature photos
-    </div>
+    <>
+      <div>
+        Nature photos
+      </div>
+      <div>
+          {
+            photos.map((photo) => 
+                <div key={photo.id}>
+                  {photo.id}
+                </div>
+              )
+          }
+      </div>
+    </>
   );
 }
 
